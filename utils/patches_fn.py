7d3581a4bf6675b2b18042b4a5317ca98907aa39
dhavalkhunt7 <dhavalkhunt07@gmail.com>
2025-01-27 09:13:28 +0100

    added description

--- utils/patches_fn.py
+++ utils/patches_fn.py
@@ -1,4 +1,6 @@
-# %% managing all the imports here only
+# %% Managing all the imports here
+# Importing necessary libraries for handling file paths, medical imaging (NIfTI), data manipulation, 
+# patch creation, and visualization
 from pathlib import Path
 import nibabel as nib
 import numpy as np
@@ -6,22 +8,22 @@ import pandas as pd
 import patchify as patchify
 import matplotlib.pyplot as plt
 
-# %% get data from imagesTr
+# %% Paths for the dataset and output
+# Define the dataset path for 3D images and output path for reconstructed data
 dataset_path = Path("../nnUNet_raw_data_base/nnUNet_raw_data/Task646_combined_patch/result_3d")
 output_path = "../nnUNet_raw_data_base/nnUNet_raw_data/Task646_combined_patch/result_3d_reconstructed"
 
-# %%
+# %% Identify and filter files based on naming conventions
+# Iterate through all .nii.gz files in the dataset folder
+# Print names of files starting with "Human"
 for i in dataset_path.glob("*.nii.gz"):
-    # print(i.name)
-    # if i.name starts with Human the add it to humna_dict else add it to rat_dict
     if i.name.startswith("Human"):
         print(i.name)
 
-
-# %% create a function to process all the data using dictionary
+# %% Function to process data and organize it into dictionaries based on file names
 def process_all_data(file):
     if i.name.startswith("Human"):
-        # use the normal code for Human_name and Human_dict
+        # Process files for Human dataset
         Human_name = i.name.split("_")[0]
         if Human_name not in Human_dict:
             Human_dict[Human_name] = {}
@@ -31,7 +33,7 @@ def process_all_data(file):
         Human_dict[Human_name][get_patch] = i
         print("patch added in %s dictionary: %s" % (Human_name, get_patch))
     elif i.name.startswith("Rat"):
-        # use the normal code for Rat_name and Rat_dict
+        # Process files for Rat dataset
         Rat_name = i.name.split("_")[0]
         if Rat_name not in Rat_dict:
             Rat_dict[Rat_name] = {}
@@ -41,59 +43,24 @@ def process_all_data(file):
         Rat_dict[Rat_name][get_patch] = i
         print("patch added in %s dictionary: %s" % (Rat_name, get_patch))
 
-    # # normal code
-    # main_name = i.name.split("_")[0]
-    # # if main name doesn't exist in dictionary then create a new key
-    # if main_name not in data_dict:
-    #     data_dict[main_name] = {}
-    #     print("dict created: %s" % main_name)
-    #
-    # get_patch = i.name.split("_")[1].split(".")[0]
-    # # if get_patch_no has one character then add 0 as prefix
-    # if len(get_patch) == 1:
-    #     get_patch = "0" + get_patch
-    # # add the patch number to the dictionary
-    # data_dict[main_name][get_patch] = i
-    # # print patch added in main_name dictionary
-    # print("patch added in %s dictionary: %s" % (main_name, get_patch))
-
-
-# # %% create a function to process all the data using dictionary
-# def process_all_data(file):
-#     main_name = i.name.split("_")[0]
-#     # if main name doesn't exist in dictionary then create a new key
-#     if main_name not in data_dict:
-#         data_dict[main_name] = {}
-#         print("dict created: %s" % main_name)
-#
-#     get_patch = i.name.split("_")[1].split(".")[0]
-#     # if get_patch_no has one character then add 0 as prefix
-#     if len(get_patch) == 1:
-#         get_patch = "0" + get_patch
-#     # add the patch number to the dictionary
-#     data_dict[main_name][get_patch] = i
-#     # print patch added in main_name dictionary
-#     print("patch added in %s dictionary: %s" % (main_name, get_patch))
-
-
-# %%
-# save the data in a dictionary
+# %% Initialize dictionaries for storing Human and Rat data
 Human_dict = {}
 Rat_dict = {}
-# data_dict = {}
+
+# Process all NIfTI files in the dataset folder
 for i in dataset_path.glob("*.nii.gz"):
     process_all_data(i)
 
-# %% dict to dataframe
+# %% Convert dictionaries to DataFrames
+# Human and Rat data are converted into Pandas DataFrames for easier manipulation and analysis
 human_df = pd.DataFrame(Human_dict)
 rat_df = pd.DataFrame(Rat_dict)
 
-# %% sort the dataframe by index
+# %% Sort DataFrames by their indices
 human_df = human_df.sort_index()
 rat_df = rat_df.sort_index()
 
-
-# %% create a function for upper code
+# %% Function to extract data arrays from NIfTI files
 def get_data_from_nifti(file_list):
     new_list = []
     for element in file_list:
@@ -102,90 +69,50 @@ def get_data_from_nifti(file_list):
         new_list.append(e_data)
     return new_list
 
-
-# %% create a function for whole process for rat
-def reconstruct_patches_rat(patches_list, ):
+# %% Function to reconstruct patches for Rat dataset
+def reconstruct_patches_rat(patches_list):
     patches = np.array(patches_list)
     patches_reshaped = patches.reshape(4, 4, 6, 45, 45, 45)
     reconstructed_data = patchify.unpatchify(patches_reshaped, (90, 90, 120))
     return reconstructed_data
 
-
-# %% create a function for whole process for human
-def reconstruct_patches_human(patches_list, ):
+# %% Function to reconstruct patches for Human dataset
+def reconstruct_patches_human(patches_list):
     patches = np.array(patches_list)
     patches_reshaped = patches.reshape(6, 7, 6, 45, 45, 45)
     reconstructed_data = patchify.unpatchify(patches_reshaped, (120, 135, 120))
     return reconstructed_data
 
-
-# %% access all the colums of dataframe and get the data # code 5
-# for i in df.columns:
-#     # get the data from dataframe
-#     name = i
-#     df_data = df[i].tolist()
-#     # get the data from nifti files
-#     data_list = get_data_from_nifti(df_data)
-#     # reconstruct patches
-#     inverse_patch = reconstruct_patches(data_list)
-#
-#     # save inverse_patch as nifti file
-#     img = nib.Nifti1Image(inverse_patch, np.eye(4))
-#     nib.save(img, output_path + "/" + name + ".nii.gz")
-#     print("saved: %s" % name)
-
-# %% access all the columns of dataframe and get the data for human and rat using code 5
+# %% Process and save reconstructed data for Human and Rat datasets
+# For Human dataset
 for i in human_df.columns:
-    # get the data from dataframe
     name = i
     df_data = human_df[i].tolist()
-    # get the data from nifti files
     data_list = get_data_from_nifti(df_data)
-    # reconstruct patches
     inverse_patch = reconstruct_patches_human(data_list)
-
-    # save inverse_patch as nifti file
     img = nib.Nifti1Image(inverse_patch, np.eye(4))
     nib.save(img, output_path + "/" + name + ".nii.gz")
     print("saved: %s" % name)
 
+# For Rat dataset
 for i in rat_df.columns:
-    # get the data from dataframe
     name = i
     df_data = rat_df[i].tolist()
-    # get the data from nifti files
     data_list = get_data_from_nifti(df_data)
-    # reconstruct patches
     inverse_patch = reconstruct_patches_rat(data_list)
-
-    # save inverse_patch as nifti file
     img = nib.Nifti1Image(inverse_patch, np.eye(4))
     nib.save(img, output_path + "/" + name + ".nii.gz")
     print("saved: %s" % name)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# %%
+# %% Process a specific NIfTI file
+# Load, crop, and preprocess data from an ADC NIfTI file
 original_adc = nib.load('input/Masked_ADC.nii')
 original_adc_data = np.array(original_adc.dataobj)
 
+# Crop data and handle NaN values
 modified_original_adc_data = original_adc_data[0:90, 0:90, 0:120]
 modified_original_adc_data[np.isnan(modified_original_adc_data)] = 0
 
-# %%check equality of reconstructed_adc and modified_original_adc_data
+# %% Check equality between reconstructed and modified data
+# Validate that the reconstructed data matches the modified original data
 print(np.array_equal(reconstructed_adc, modified_original_adc_data))
